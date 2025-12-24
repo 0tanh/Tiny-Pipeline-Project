6f1a5bd47efc4e5d5f49e1aaebc9612651ea5834
Betty <bettypotgieter@Bettys-MacBook-Air.local>
2025-12-24 08:19:56 +0200

    added storybook to try and test this single API component

--- taskbox/vite.config.ts
+++ taskbox/vite.config.ts
@@ -2,11 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
@@ -26,12 +25,12 @@ export default defineConfig({
         browser: {
           enabled: true,
           headless: true,
-          provider: playwright({}),
+          provider: 'playwright',
           instances: [{
             browser: 'chromium'
           }]
         },
-        setupFiles: ['.storybook/vitest.setup.js']
+        setupFiles: ['.storybook/vitest.setup.ts']
       }
     }]
   }
