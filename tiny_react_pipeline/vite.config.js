5116c771e8d03aaaca960c303a341231c3b9996e
Betty <bettypotgieter@Bettys-MacBook-Air.local>
2025-12-24 16:56:24 +0200

    testing more commands

--- tiny_react_pipeline/vite.config.js
+++ tiny_react_pipeline/vite.config.js
@@ -12,7 +12,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
-  base: "/Tiny-Pipeline-Project/",
+  base: "/",
   server: {
     proxy: {
       '/api': {
